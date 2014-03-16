7efff561d4d777daa92bd6b0c9b9e2ff7d47ceec
Derek Razo <derekrazo@gmail.com>
2014-03-16 02:39:35 -0700

    added sidebar items w/ style

--- client/reddit-redesign.js
+++ client/reddit-redesign.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
     $('#hot, #new, #top').click(function(event) {
-      $('#hot').removeClass("active");
-      $('#top').removeClass("active");
-      $('#new').removeClass("active");
-      $(this).toggleClass("active");
+      $('#hot').removeClass("active-button");
+      $('#top').removeClass("active-button");
+      $('#new').removeClass("active-button");
+      $(this).toggleClass("active-button");
     });
+
+    $('.sidebar-search-bar').hover(function() {
+      $('.sidebar-search-icon').addClass('opacity-up');
+    });
+
+
+    $('.sidebar-search-bar-container').hover(function() {
+      $('.sidebar-search-icon').removeClass('opacity-up');
+      $('.sidebar-search-bar').removeClass('active-search-bar');
+    });
+
+
+
+
+
+
 });
+
+
