86323e49a1c0c22f749a2d3927c8c5ba2f470aa2
Derek Razo <derekrazo@gmail.com>
2014-03-20 01:49:43 -0700

    added box shadows, updated left sidebar and items, made right bar, added login to nav bar

--- client/reddit-redesign.js
+++ client/reddit-redesign.js
@@ -6,23 +6,46 @@ $(document).ready(function(){
       $('#top').removeClass("active-button");
       $('#new').removeClass("active-button");
       $(this).toggleClass("active-button");
+      console.log("hi");
 
     });
 
-    $('.sidebar-search-bar').hover(function() {
-      $('.sidebar-search-icon').addClass('opacity-up');
+  $('.left-sidebar-item-container').click(function(event) {
+       $('.left-sidebar-item-tab').removeClass('active-tab-orange');
+     });
+
+
+    $('.left-sidebar-search-bar').hover(function() {
+      $('.left-sidebar-search-icon').addClass('opacity-up');
     });
 
 
-    $('.sidebar-search-bar-container').hover(function() {
-      $('.sidebar-search-icon').removeClass('opacity-up');
-      $('.sidebar-search-bar').removeClass('active-search-bar');
+    $('.left-sidebar-search-bar-container').hover(function() {
+      $('.left-sidebar-search-icon').removeClass('opacity-up');
+      $('.left-sidebar-search-bar').removeClass('active-search-bar');
     });
 
-    $('.sidebar-item-container').click(function() {
-      console.log(this);
-      $('.sidebar-item-tab').removeClass('active-tab');
-      $(this).children('.sidebar-item-tab').addClass('active-tab');
+    $('.left-sidebar-item-container').click(function() {
+      $('.left-sidebar-item-tab').removeClass('active-tab');
+      $(this).children('.left-sidebar-item-tab').addClass('active-tab');
+    });
+
+
+
+
+    $('.right-sidebar-search-bar').hover(function() {
+      $('.right-sidebar-search-icon').addClass('opacity-up');
+    });
+
+    $('.right-sidebar-search-bar-container').hover(function() {
+      $('.right-sidebar-search-icon').removeClass('opacity-up');
+      $('.right-sidebar-search-bar').removeClass('active-search-bar');
+    });
+
+    $('.top').click(function(event) {
+      $('.top').removeClass("active-tab-orange");
+      $('.left-sidebar-item-tab').removeClass("active-tab");
+      $(this).children('.left-sidebar-item-tab').addClass('active-tab-orange');
     });
 
 
